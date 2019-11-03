3d761cc2f8c8431b07067cfd125200f3088a5215
Mickey <Micky40086@gmail.com>
2019-11-03 18:37:15 +0800

    feat: add products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  ['apple', 50],
+  ['banana', 10]
+].each do |x, y|
+  Product.find_or_create_by(:name => x) do |product|
+    product.price = 10
+  end
+end
